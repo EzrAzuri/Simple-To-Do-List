06a263e69eec49f0431ade8d5853ee20aa9e0eea
BlackDeath <muhduzairfahimie@gmail.com>
2020-05-17 16:59:57 +0800

    Adding Node Module Exports

--- app.js
+++ app.js
@@ -2,30 +2,39 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("en-US", options);
-    res.render("list", {kindOfDay: day, newItem: items});
+    let day = date.getDate();
+    res.render("list", {listTitle: day, newItem: items});
 });
 
 app.post("/", function(req, res){
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }  
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work", newItem: workItems});
+}); 
+
+app.get("/about", function(req, res){
+    res.render("about");
 });
 
 app.listen(3000, function() { 
